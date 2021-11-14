dc08f0fee522f59b90b9edf0a08fdad41f9d48c1
yoshikouki <yoshikouki@gmail.com>
2021-11-14 23:30:35 +0900

    ユーザー作成用コンポーネントに切り出し

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,33 +1,9 @@
 import Head from 'next/head'
 import {PrismaClient} from '@prisma/client'
-import React, {useState} from "react";
+import React from "react";
+import {SignUp} from "../../components/SignUp";
 
-interface Props {
-  users: User[]
-}
-
-interface User {
-  id: number
-  email: string
-  name: string
-}
-
-const Users = (props: Props) => {
-  const [users, setUsers] = useState(props.users)
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-
-  const submit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({email: email, name: name}),
-    })
-    const response = await fetch('/api/users')
-    const newUsers = await response.json()
-    setUsers(newUsers)
-  }
-
+const Users = () => {
   return (
     <>
       <Head>
@@ -37,47 +13,7 @@ const Users = (props: Props) => {
       </Head>
 
       <main>
-        <h1>Users</h1>
-        <form onSubmit={submit}>
-          <div className="form-item">
-            <label>
-              メールアドレス
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-          </div>
-          <div className="form-item">
-            <label>
-              表示名
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </label>
-          </div>
-          <input type="submit" value='作成'/>
-        </form>
-
-        <table>
-          <tr>
-            <th>id</th>
-            <th>email</th>
-            <th>name</th>
-          </tr>
-          {users.map(user => {
-            return (
-              <tr key="users-table-row-{user.id}">
-                <td>{user.id}</td>
-                <td>{user.email}</td>
-                <td>{user.name}</td>
-              </tr>
-            )
-          })}
-        </table>
+        <SignUp />
       </main>
     </>
   )
