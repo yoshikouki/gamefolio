54ae187b4eb1fa9185b06f99fd4fc781a8170505
yoshikouki <yoshikouki@gmail.com>
2021-11-19 00:04:03 +0900

    lint

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import React, { ReactNode } from 'react'
+import React, {ReactNode} from 'react'
 import {styled} from "@mui/system";
 
 export const DefaultLayout = (props: { title?: string; children: ReactNode }) => {
@@ -7,8 +7,8 @@ export const DefaultLayout = (props: { title?: string; children: ReactNode }) =>
     <>
       <Head>
         <title>{props.title || 'Gamefolio'}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="initial-scale=1, width=device-width"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       </Head>
