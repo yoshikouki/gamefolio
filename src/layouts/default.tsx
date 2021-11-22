3667af6939668544b127bb7032486b37009721a2
yoshikouki <yoshikouki@gmail.com>
2021-11-22 22:13:21 +0900

    lint

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -11,8 +11,6 @@ export const DefaultLayout = (props: { title?: string; children: ReactNode }) =>
         <title>{props.title || 'Gamefolio'}</title>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="initial-scale=1, width=device-width"/>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
       </Head>
 
       <TopNavigation/>
